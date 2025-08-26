2b5064ba1d32d93c15e55132cba8b24bf0acb6b3
Aurora Simão <user@Aurora-MBP.lan>
2025-08-26 18:41:40 +0100

    theme-toggle

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -12,15 +12,13 @@ export function ThemeToggle() {
       variant="ghost"
       size="sm"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-<<<<<<< HEAD
       className="w-100% justify-start"
-=======
-
->>>>>>> Aurora
     >
-      <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      <span className="ml-2">Toggle theme</span>
+      <>
+        <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <span className="ml-2">Toggle theme</span>
+      </>
     </Button>
   )
 }
